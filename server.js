7bc9e45cdda08b236c041b6a9592954f3f13651f
Yubo <r00631026@ntu.edu.tw>
2015-07-17 16:34:35 +0800

    add onRequest

--- server.js
+++ server.js
@@ -1,6 +1,21 @@
 var http = require('http')
 var port = process.env.PORT || 1337;
+
+function start(){
+	function onRequest(request, response){
+		console.log("request for "+ pathname + "received.");
+		response.writeHead(200,{"Content-Type": "text/plain"});
+		response.write("Hello Gungerger");
+		response.end;
+	}
+
+	http.createServer(onRequest).listen(port);
+	console.log("Server has started.");
+}
+
+/*
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello Gungerger\n');
 }).listen(port);
+*/
