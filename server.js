43022b03d2d6a9f3dfbcb45e77f331bea2225382
Yubo <r00631026@ntu.edu.tw>
2015-07-18 10:57:11 +0800

    add bb.js

--- server.js
+++ server.js
@@ -1,12 +1,5 @@
-var http = require('http')
-var port = process.env.PORT || 1337;
-
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello Gungerger\n');
-  console.log("Server has started");
-}).listen(port);
-
+var b = require("./bb");
+bb.start();
 /*
 function onRequest(request,response){
 
