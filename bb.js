0bc8ea26c070a5541e395d7832853f2da8e32101
Yubo <r00631026@ntu.edu.tw>
2015-07-18 15:33:00 +0800

    whay

--- bb.js
+++ bb.js
@@ -1,24 +1,6 @@
-//var http = require('http')
-//var port = process.env.PORT || 1337;
-
 var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
 }).listen(port);
-
-
-/*
-function onRequest(request,response){
-
-			console.log("request for " + pathname + "received.");
-			response.writeHead(200,{'Content-Type': 'text/plain'});
-			response.end('Hello World\n');
-		}
-function start(){
-		
-		http.createServer(onRequest).listen(port);
-	}
-start();
-*/
--- server.js
+++ server.js
@@ -1,19 +1,8 @@
-
-var bb = require('./bb.js');
 var http = require('http')
 var port = process.env.PORT || 1337;
-bb.start();
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
 
-/*
-function onRequest(request,response){
 
-			console.log("request for " + pathname + "received.");
-			response.writeHead(200,{'Content-Type': 'text/plain'});
-			response.end('Hello World\n');
-		}
-function start(){
-		
-		http.createServer(onRequest).listen(port);
-	}
-start();
-*/
