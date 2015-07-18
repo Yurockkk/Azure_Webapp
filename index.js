b1ae25df18a04a335dd89b6873358f6e8012966d
Yubo <r00631026@ntu.edu.tw>
2015-07-18 10:53:46 +0800

    index

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Gungerger\n');
+  console.log("Server has started");
+}).listen(port);
+
+/*
+function onRequest(request,response){
+
+			console.log("request for " + pathname + "received.");
+			response.writeHead(200,{'Content-Type': 'text/plain'});
+			response.end('Hello World\n');
+		}
+function start(){
+		
+		http.createServer(onRequest).listen(port);
+	}
+start();
+*/
