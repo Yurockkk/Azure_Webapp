84ef503d5c3ba755ad4af54343094f17e27821fb
Yubo <r00631026@ntu.edu.tw>
2015-07-23 15:14:44 +0800

    hh

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
 	var type = 'Content-Type': 'text/plain';
-	var output = ;
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port);
