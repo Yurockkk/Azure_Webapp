86cfe14ffda0f0c5421808150dad0d351657012e
Yubo <r00631026@ntu.edu.tw>
2015-07-23 15:26:08 +0800

    baba

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
 var http = require('http');
+var url = require('url');
 var port = process.env.port || 1337;
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+	var pathname = url.parse(req.url).pathname;
+	console.log('Received '+pathname +'request');
+	var routes={
+		'/': function(req,res){
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+   			res.end('Hello World\n');
+		},
+		'/login': function(req,res){
+			res.writeHead(200, { 'Content-Type': 'text/plain' });
+   			res.end('Hello Gunger\n');
+		},
+		'/banana':function(req,res){
+			res.sendfile('testtest.html')
+		}
+	};
+
+	if(typeof routes[pathname] === 'function'){
+		routes[pathname](req,res);
+	}else{
+		res.writeHead(404);
+   		res.end('404 not found\n');
+	}
+
 }).listen(port);
