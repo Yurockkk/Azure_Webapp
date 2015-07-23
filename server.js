f43bbd138bf5a283f8592adaa4e9d3a3209ad2d5
Yubo <r00631026@ntu.edu.tw>
2015-07-23 15:09:37 +0800

    banana

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-var app = require('./app');
+
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+	var type = 'Content-Type': 'text/plain';
+	var output = ;
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
