40c2d60855ca0710ebe69ece666e93b9500ae192
Yubo <r00631026@ntu.edu.tw>
2015-07-23 15:34:20 +0800

    exprsss

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var http = require('http');
 var url = require('url');
+var express = require('express');
+var app = express();
 var port = process.env.port || 1337;
+
 http.createServer(function (req, res) {
 	var pathname = url.parse(req.url).pathname;
 	console.log('Received '+pathname +'request');
+
+	app.get('/',function(req,res){
+		res.send('Hello World');
+	});
+
+	app.get('/login',function(req,res){
+		res.send('Hello Gunger');
+	});
+
+	app.get('banana',function(req,res){
+		res.sendfile(testtest.html);
+	})
+
+	/*
 	var routes={
 		'/': function(req,res){
 			res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -24,5 +41,6 @@ http.createServer(function (req, res) {
 		res.writeHead(404);
    		res.end('404 not found\n');
 	}
+	*/
 
 }).listen(port);
