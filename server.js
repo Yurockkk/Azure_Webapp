3944672268172caa8c250e82d8206342c00d1d47
Yubo <r00631026@ntu.edu.tw>
2015-07-24 11:45:25 +0800

    yy

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ console.log('mongoose setup connectionString');
 var Schema = mongoose.Schema;
 
 var Todo = new Schema({
-	user_id		: String,
-	content 	: String,
-	updated	_at : Date
+    user_id    : String,
+    content    : String,
+    updated_at : Date
 });
 
 console.log('mongoose setup Schema');
