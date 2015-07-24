300b0a0932a6ce52badc752ea768bfab8b40f359
Yubo <r00631026@ntu.edu.tw>
2015-07-24 15:55:30 +0800

    add globle

--- server.js
+++ server.js
@@ -27,8 +27,8 @@ mongoose.connect(connectionString,function(err){
 });
 console.log('mongoose setup model and connetion');
 
-
-
+	var user = new User();
+	
 
 	app.set('port',process.env.port || 1337);
 	app.get('/',function(req,res){
@@ -51,7 +51,7 @@ console.log('mongoose setup model and connetion');
 	app.get('/create',function(req,res){
 		console.log("In /create, name= ", req.query.name);
 		console.log("In /create, phone= ", req.query.phone);
-		var user = new User();
+		//var user = new User();
 		user.name=req.query.name;
 		user.phone=req.query.phone;
 		user.save(function( err, user, count ){
