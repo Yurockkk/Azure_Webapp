5dddd41be2a44b91958df5ca2a93cb3ce29d1129
Yubo <r00631026@ntu.edu.tw>
2015-07-28 13:24:04 +0800

    try tasklist_rename

---
+++ app.js
@@ -0,0 +1,79 @@
+var TaskList = require('./routes/tasklist');
+var taskList = new TaskList(process.env.CUSTOMCONNSTR_MONGOLAB_URI);
+
+console.log("In app.js");
+
+var http = require('http');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+//port setup
+app.set('port', process.env.port || 1337);
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//app.get('/', taskList.showTasks.bind(taskList));
+app.get('/', function(req,res){
+
+  res.send('hello,gunger');
+});
+app.post('/addtask', taskList.addTask.bind(taskList));
+app.post('/completetask', taskList.completeTask.bind(taskList));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+/*
+http.createServer(app).listen(app.get('port'), function(req, res) {
+    console.log("express server listen to " + app.get('port'));
+});
+*/
+app.listen(app.get('port')); // Listen on port 3000
+//module.exports = app;
+//module.exports = app;
