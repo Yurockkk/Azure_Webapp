0513aaa6dcfed0dcd19a07fd25e327f7309608a9
Yubo <r00631026@ntu.edu.tw>
2015-07-31 14:41:55 +0800

    add /extract/category

--- routes/extract.js
+++ routes/extract.js
@@ -12,8 +12,8 @@ router.get('/', function(req, res, next) {
   	});
 });
 
-router.get('/:itemCategory', function(req, res, next) {
-	console.log("In /extract/:itemCategory= "+req.params.itemCategory);
+router.get('/category/:itemCategory', function(req, res, next) {
+	console.log("In /extract/category/:itemCategory= "+req.params.itemCategory);
 	console.log(" "+req.params.itemCategory);
 	if(req.params.itemCategory){
 		task.find({itemCategory: req.params.itemCategory},function(err,docs){
@@ -21,13 +21,18 @@ router.get('/:itemCategory', function(req, res, next) {
   		});
 	}
 
+  	
+});
+
+router.get('/completed/:itemCompleted',function(req,res,next){
+	console.log("In /extract/completed/:itemCompleted= "+req.params.itemCompleted);
+	
 	if(req.params.itemCompleted){
 		task.find({itemCompleted: req.params.itemCompleted},function(err,docs){
   		res.json(docs);
   		});
   	}
-  	
-});
+})
 
 
 
