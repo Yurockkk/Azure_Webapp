8dba918280fe6e7ca434afa94a04b756c03a2cf0
Yubo <r00631026@ntu.edu.tw>
2015-08-04 10:24:34 +0800

    add url

--- app.js
+++ app.js
@@ -68,7 +68,29 @@ app.get('/push',function(req,res){
     messages_type: 1,
     push_type: 1,
     user_id: userId,
-    messages: ["hello"],
+    messages: [{
+
+      //android必选，ios可选
+      "title" : "hello" ,   
+      “description: "hello world" 
+
+      //android特有字段，可选 
+      "notification_builder_id": 0,
+      "notification_basic_style": 7,
+      "open_type":0,
+      "net_support" : 1,
+      "user_confirm": 0,
+      "url": "http://developer.baidu.com",
+      "pkg_content":"",
+      "pkg_name" : "com.baidu.bccsclient",
+      "pkg_version":"0.1",
+
+      //android自定义字段
+      "custom_content": {
+        "Google":"https://www.google.com.tw/?gws_rd=ssl", 
+        "key2":"value2"
+        }
+  }],
     msg_keys: ["title"]
   };
   console.log('data setting');
